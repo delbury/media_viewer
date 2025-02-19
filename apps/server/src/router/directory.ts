954cbf4507357748a053936ac2e5932428a89f1e
delbury <592804814@qq.com>
2025-02-19 22:50:57 +0800

    chore: server project add path alias config

--- apps/server/src/router/directory.ts
+++ apps/server/src/router/directory.ts
@@ -1,9 +1,9 @@
+import { ApiResponseBase } from '#pkgs/shared';
 import Router from '@koa/router';
 
 const directoryRouter = new Router({ prefix: '/dir' });
 
 directoryRouter.get('/tree', async ctx => {
-  ctx.status = 400;
   ctx.body = {
     error: 'this is not ok',
     msg: 'ok',
