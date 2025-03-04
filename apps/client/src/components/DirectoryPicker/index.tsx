2e2efe7964a514bb1eae4456ca7e18d9aa9f7ac0
delbury <592804814@qq.com>
2025-03-04 21:39:46 +0800

    feat: optimize dir picker

--- apps/client/src/components/DirectoryPicker/index.tsx
+++ apps/client/src/components/DirectoryPicker/index.tsx
@@ -5,29 +5,24 @@ import { useConfirmDialog } from '@/hooks/useConfirmDialog';
 import { useDialogState } from '@/hooks/useDialogState';
 import { DirTreeData, useSwr } from '@/hooks/useSwr';
 import { formatDate } from '@/utils';
-import { FolderOutlined, LoopOutlined, OtherHousesOutlined } from '@mui/icons-material';
+import { FolderOutlined, LoopOutlined, OtherHouses, OtherHousesOutlined } from '@mui/icons-material';
 import {
   Box,
   Breadcrumbs,
   Button,
   Chip,
+  Divider,
   IconButton,
   List,
   ListItemButton,
   ListItemIcon,
   ListItemText,
 } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import { useTranslations } from 'next-intl';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import Empty from '../Empty';
 import style from './index.module.scss';
-
-const useStyles = makeStyles({
-  ol: {
-    rowGap: '8px',
-  },
-});
+import { useStyles } from './style';
 
 const SEPARATOR = '/';
 
@@ -39,30 +34,31 @@ export default function DirectoryPicker() {
   // 后端强制更新文件信息
   const updateRequest = useSwr('dirUpdate', { lazy: true });
   // 二次确认是否刷新
-  const { dialog: confirmDialog, handleOpenSkipConfirm: confirmOpen } = useConfirmDialog(updateRequest.refresh);
+  const { dialog: confirmDialog, handleOpen: confirmOpen } = useConfirmDialog(updateRequest.refresh);
 
   // 请求文件夹树
   const dirRequest = useSwr('dirTree');
   // example: /a/b/c
-  const [pathList, setPathList] = useState<string[]>([]);
+  const [pathList, setPathList] = useState<DirTreeData[]>([]);
   // 当前目录下的子文件夹
-  const [currentDirs, setCurrentDirs] = useState<DirTreeData[]>([]);
+  const currentPathNode = useMemo(() => pathList[pathList.length - 1], [pathList]);
+  const currentDirs = useMemo(() => currentPathNode?.children ?? [], [currentPathNode]);
+  const isAtHome = pathList.length === 1;
 
+  // 设置当前已选文件夹
+  const setTarget = (index: number) => {
+    setPathList(pathList.slice(0, index + 1));
+  };
   // 初始化数据
   useEffect(() => {
-    setCurrentDirs(dirRequest.data?.children ?? []);
-    setPathList([]);
+    setPathList(dirRequest.data ? [dirRequest.data] : []);
   }, [dirRequest.data]);
   useEffect(() => {
-    if (updateRequest.data) {
-      setCurrentDirs(updateRequest.data?.treeNode?.children ?? []);
-      setPathList([]);
-    }
+    setPathList(updateRequest.data?.treeNode ? [updateRequest.data.treeNode] : []);
   }, [updateRequest.data]);
 
   const handleSelectChild = (dir: DirTreeData) => {
-    setPathList([...pathList, dir.name]);
-    setCurrentDirs(dir.children ?? []);
+    setPathList([...pathList, dir]);
   };
 
   const handleOk = useCallback(() => {
@@ -94,71 +90,89 @@ export default function DirectoryPicker() {
             </IconButton>
           }
         >
-          {/* 已选文件夹 */}
-          <Breadcrumbs
-            separator={SEPARATOR}
-            classes={{ ol: classes.ol }}
-          >
-            <Box sx={{ fontSize: 0 }}>
-              <OtherHousesOutlined fontSize="small" />
-            </Box>
-            {pathList.map((path, index) => {
-              const isLast = index === pathList.length - 1;
-              return (
-                <Box
-                  className={style.path_btn}
-                  key={`${path}_${index}`}
+          <Box height={'50vh'}>
+            {/* 已选文件夹 */}
+            <Breadcrumbs
+              separator={SEPARATOR}
+              classes={{ ol: classes.ol }}
+            >
+              {pathList.map((path, index) => {
+                const isFirst = index === 0;
+                const isLast = index === pathList.length - 1;
+                const key = `${path}_${index}`;
+                return isFirst ? (
+                  <Box
+                    sx={{ fontSize: 0 }}
+                    key={key}
+                  >
+                    <IconButton
+                      size="small"
+                      color={isAtHome ? 'primary' : void 0}
+                      onClick={() => setTarget(index)}
+                    >
+                      {isAtHome ? <OtherHouses /> : <OtherHousesOutlined />}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <Box
+                    className={style.path_btn}
+                    key={key}
+                  >
+                    <Chip
+                      label={path.name}
+                      size="small"
+                      variant={isLast ? 'filled' : 'outlined'}
+                      color={isLast ? 'primary' : 'default'}
+                      onClick={() => setTarget(index)}
+                      style={{ maxWidth: 'min(300px, 40vw)' }}
+                    />
+                  </Box>
+                );
+              })}
+            </Breadcrumbs>
+
+            <Divider style={{ marginTop: 8 }} />
+
+            {/* 当前文件夹的子文件夹 */}
+            <List>
+              {currentDirs.map(dir => (
+                <ListItemButton
+                  key={dir.name}
+                  onClick={() => handleSelectChild(dir)}
                 >
-                  <Chip
-                    label={path}
-                    size="small"
-                    variant={isLast ? 'filled' : 'outlined'}
-                    color={isLast ? 'primary' : 'default'}
+                  <ListItemIcon>
+                    <FolderOutlined fontSize="large" />
+                  </ListItemIcon>
+
+                  <ListItemText
+                    primary={dir.name}
+                    secondary={
+                      <>
+                        <span>{formatDate(dir.updated)}</span>
+                        <span>{dir.files?.length} files</span>
+                      </>
+                    }
+                    slotProps={{
+                      primary: {
+                        sx: {
+                          overflow: 'hidden',
+                          whiteSpace: 'nowrap',
+                          textOverflow: 'ellipsis',
+                        },
+                      },
+                      secondary: {
+                        sx: {
+                          display: 'flex',
+                          justifyContent: 'space-between',
+                        },
+                      },
+                    }}
                   />
-                </Box>
-              );
-            })}
-          </Breadcrumbs>
-
-          {/* 当前文件夹的子文件夹 */}
-          <List>
-            {currentDirs.map(dir => (
-              <ListItemButton
-                key={dir.name}
-                onClick={() => handleSelectChild(dir)}
-              >
-                <ListItemIcon>
-                  <FolderOutlined fontSize="large" />
-                </ListItemIcon>
-
-                <ListItemText
-                  primary={dir.name}
-                  secondary={
-                    <>
-                      <span>{formatDate(dir.updated)}</span>
-                      <span>{dir.files?.length} files</span>
-                    </>
-                  }
-                  slotProps={{
-                    primary: {
-                      sx: {
-                        overflow: 'hidden',
-                        whiteSpace: 'nowrap',
-                        textOverflow: 'ellipsis',
-                      },
-                    },
-                    secondary: {
-                      sx: {
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                      },
-                    },
-                  }}
-                />
-              </ListItemButton>
-            ))}
-            {!currentDirs.length && <Empty label={t('Tools.NoDirectories')} />}
-          </List>
+                </ListItemButton>
+              ))}
+              {!currentDirs.length && <Empty label={t('Tools.NoDirectories')} />}
+            </List>
+          </Box>
 
           {confirmDialog}
         </Dialog>
---
+++ apps/client/src/components/DirectoryPicker/style.ts
@@ -0,0 +1,8 @@
+// import { styled } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles({
+  ol: {
+    rowGap: '8px',
+  },
+});
