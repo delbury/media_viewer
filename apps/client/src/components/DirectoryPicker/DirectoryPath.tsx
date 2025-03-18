c04dafe24f15ff13cbd037d1a4b9df212a0c9036
delbury <xxx@yy.zz>
2025-03-18 22:32:28 +0800

    refactor: dir picker

---
+++ apps/client/src/components/DirectoryPicker/DirectoryPath.tsx
@@ -0,0 +1,61 @@
+import { Box, Breadcrumbs, Chip, IconButton } from '@mui/material';
+import { useStyles } from './style';
+import { OtherHouses, OtherHousesOutlined } from '@mui/icons-material';
+import { DirectoryInfo } from '@shared';
+import style from './index.module.scss';
+
+const SEPARATOR = '/';
+
+interface DirectoryPathProps {
+  pathList: DirectoryInfo[];
+  onItemClick?: (index: number) => void;
+}
+
+const DirectoryPath = ({ pathList, onItemClick }: DirectoryPathProps) => {
+  const classes = useStyles();
+
+  const isAtHome = pathList.length === 1;
+
+  return (
+    <Breadcrumbs
+      separator={SEPARATOR}
+      classes={{ ol: classes.ol }}
+    >
+      {pathList.map((path, index) => {
+        const isFirst = index === 0;
+        const isLast = index === pathList.length - 1;
+        const key = `${path}_${index}`;
+        return isFirst ? (
+          <Box
+            sx={{ fontSize: 0 }}
+            key={key}
+          >
+            <IconButton
+              size="small"
+              color={isAtHome ? 'primary' : void 0}
+              onClick={() => onItemClick?.(index)}
+            >
+              {isAtHome ? <OtherHouses /> : <OtherHousesOutlined />}
+            </IconButton>
+          </Box>
+        ) : (
+          <Box
+            className={style.path_btn}
+            key={key}
+          >
+            <Chip
+              label={path.name}
+              size="small"
+              variant={isLast ? 'filled' : 'outlined'}
+              color={isLast ? 'primary' : 'default'}
+              onClick={() => onItemClick?.(index)}
+              style={{ maxWidth: 'min(300px, 35vw)' }}
+            />
+          </Box>
+        );
+      })}
+    </Breadcrumbs>
+  );
+};
+
+export default DirectoryPath;
