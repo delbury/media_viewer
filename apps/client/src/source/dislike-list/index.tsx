b872a4fc77ffb05429baa0d8b8811bbf2cf41e94
delbury <592804814@qq.com>
2025-05-21 20:26:04 +0800

    feat: dislike file list support dir filter

--- apps/client/src/source/dislike-list/index.tsx
+++ apps/client/src/source/dislike-list/index.tsx
@@ -2,15 +2,28 @@
 
 import FileDetailDialog from '#/components/DirectoryPicker/components/FileDetailDialog';
 import { FileListContent } from '#/components/FileListPreviewer';
+import ScrollBox from '#/components/ScrollBox';
 import { useSwr } from '#/hooks/useSwr';
 import { FileInfo } from '#pkgs/apis';
+import { FILE_INFO_ID_FIELD } from '#pkgs/tools/common';
 import { isNil } from 'lodash-es';
 import { useCallback, useMemo, useState } from 'react';
 import { StyledContent, StyledDirItem, StyledDirs, StyledDislikeListWrapper } from './style';
 
 const DislikeList = () => {
   const [detailFile, setDetailFile] = useState<FileInfo | null>(null);
+  const [selectedDir, setSelectedDir] = useState<string | null>(null);
   const listRequest = useSwr('mediaDislikeList');
+
+  const selectedFileSet = useMemo(() => {
+    const set = new Set<string>();
+    listRequest.data?.list.forEach(it => {
+      if (it.showDir === selectedDir) {
+        set.add(it[FILE_INFO_ID_FIELD]);
+      }
+    });
+    return set;
+  }, [selectedDir, listRequest.data]);
 
   const dirs = useMemo(() => {
     const map = new Map<string, number>();
@@ -26,23 +39,39 @@ const DislikeList = () => {
     setDetailFile(file);
   }, []);
 
+  const handleItemClick = useCallback((file: FileInfo) => {
+    setSelectedDir(v => (v === file.showDir ? null : file.showDir));
+  }, []);
+
+  const handleDirClick = useCallback((dir: string) => {
+    setSelectedDir(v => (v === dir ? null : dir));
+  }, []);
+
   return (
     <StyledDislikeListWrapper>
       <StyledDirs>
-        {dirs.map(d => (
-          <StyledDirItem key={d[0]}>
-            {/* <ContentCopyRounded fontSize="inherit" /> */}
-            <span>{d[0]}</span>
-            <span>{d[1]}</span>
-          </StyledDirItem>
-        ))}
+        <ScrollBox sx={{ height: '100%' }}>
+          {dirs.map(d => (
+            <StyledDirItem
+              selected={selectedDir === d[0]}
+              key={d[0]}
+              onClick={() => handleDirClick(d[0])}
+            >
+              {/* <ContentCopyRounded fontSize="inherit" /> */}
+              <span>{d[0]}</span>
+              <span>{d[1]}</span>
+            </StyledDirItem>
+          ))}
+        </ScrollBox>
       </StyledDirs>
 
       <StyledContent>
         <FileListContent
           files={listRequest.data?.list ?? []}
           onImgClick={handleImgClick}
+          onItemClick={handleItemClick}
           isLoading={listRequest.isLoading}
+          selectedIdSet={selectedFileSet}
         />
       </StyledContent>
 
--- apps/client/src/source/dislike-list/style.ts
+++ apps/client/src/source/dislike-list/style.ts
@@ -7,21 +7,29 @@ export const StyledDislikeListWrapper = styled(Box)`
 `;
 
 export const StyledDirs = styled(Box)`
+  max-height: 20dvh;
+  overflow: hidden;
   margin-bottom: 8px;
   padding-bottom: 8px;
   border-bottom: 1px solid ${({ theme }) => theme.palette.divider};
 `;
 
-export const StyledDirItem = styled(Box)`
+export const StyledDirItem = styled(Box, {
+  shouldForwardProp: prop => prop !== 'selected',
+})<{ selected?: boolean }>`
   display: grid;
   grid-template-columns: 1fr auto;
   gap: 16px;
   font-size: 0.875rem;
-  color: ${({ theme }) => theme.palette.text.secondary};
   cursor: pointer;
+  text-underline-offset: 2px;
+
+  color: ${({ theme, selected }) =>
+    selected ? theme.palette.primary.light : theme.palette.text.secondary};
+
+  ${({ selected }) => (selected ? 'text-decoration: underline' : '')};
 
   :hover {
-    color: ${({ theme }) => theme.palette.text.primary};
     text-decoration: underline;
   }
 
