c7f269d79e7f649236dc982fad92bd279b9a570a
delbury <592804814@qq.com>
2025-06-23 21:16:55 +0800

    feat: optmize video range header

--- apps/server/src/util/range.ts
+++ apps/server/src/util/range.ts
@@ -1,47 +1,69 @@
 import { ERROR_MSG } from '#pkgs/i18n/errorMsg';
 import { FIFO } from '#pkgs/tools/cahceStrategy';
+import { VIDEO_REG } from '#pkgs/tools/constant';
 import { ParameterizedContext } from 'koa';
+import { clamp } from 'lodash-es';
 import mime from 'mime';
 import { createReadStream, Stats } from 'node:fs';
 import { stat } from 'node:fs/promises';
+import { getMediaDetail } from './media';
 
 const fileStatFifo = new FIFO<Stats>(8);
 
+// MB
+const MB = 2 ** 20;
+
 // 每次的最大 size，MB
-const MAX_RANGE_SIZE_STEP = 4 * 2 ** 20;
+const BASE_RANGE_SIZE_STEP = 4 * MB;
 
 // 获取文件的单次 range 大小
 const BASE_BIG_FILE_SIZE_THRESHOLD = 2 ** 30;
 // 倍数，index + 2
 const BIG_FILE_SIZE_THRESHOLD_SCALES = [1.5, 5, 10];
-const getFileStep = (fileInfo: Stats) => {
+const MAX_STEP = (BIG_FILE_SIZE_THRESHOLD_SCALES.length + 1) * BASE_RANGE_SIZE_STEP;
+const getFileStep = (fileSize: number) => {
   let multi = 1;
   for (let i = BIG_FILE_SIZE_THRESHOLD_SCALES.length; i >= 0; i--) {
-    if (fileInfo.size > BIG_FILE_SIZE_THRESHOLD_SCALES[i] * BASE_BIG_FILE_SIZE_THRESHOLD) {
+    if (fileSize > BIG_FILE_SIZE_THRESHOLD_SCALES[i] * BASE_BIG_FILE_SIZE_THRESHOLD) {
       multi = i + 2;
       break;
     }
   }
-  return multi * MAX_RANGE_SIZE_STEP;
+  return multi * BASE_RANGE_SIZE_STEP;
 };
 
 // 解析 Range 头
 export const resolveRange = async (range: string, filePath: string) => {
   const positions = range.replace(/bytes=/, '').split('-');
 
-  // 获取文件信息
-  let fileInfo = await fileStatFifo.get(filePath);
-  if (!fileInfo) {
-    try {
-      fileInfo = await stat(filePath);
-      fileStatFifo.set(filePath, fileInfo);
-    } catch {
-      throw new Error(ERROR_MSG.noFile);
-    }
-  }
+  const isVideo = VIDEO_REG.test(filePath);
+  let fileSize = 0;
   // 单个请求的最大大小
-  const sizeStep = getFileStep(fileInfo);
-  const total = fileInfo ? fileInfo.size : 0;
+  let sizeStep = BASE_RANGE_SIZE_STEP;
+
+  if (isVideo) {
+    const videoDetail = await getMediaDetail(filePath);
+    fileSize = +(videoDetail?.format.size ?? '');
+    const bitRate = Math.ceil(+(videoDetail?.format.bit_rate ?? '') / 8 / MB);
+    sizeStep = getFileStep(fileSize);
+    // 根据视频码率，限制单个请求的最大大小
+    sizeStep = clamp(bitRate, BASE_RANGE_SIZE_STEP, MAX_STEP);
+  } else {
+    // 获取文件信息
+    let fileInfo = await fileStatFifo.get(filePath);
+    if (!fileInfo) {
+      try {
+        fileInfo = await stat(filePath);
+        fileStatFifo.set(filePath, fileInfo);
+        fileSize = fileInfo.size;
+      } catch {
+        throw new Error(ERROR_MSG.noFile);
+      }
+    }
+    sizeStep = getFileStep(fileSize);
+  }
+
+  const total = fileSize;
   const start = Number(positions[0]);
   let end = Number(positions[1] || total - 1);
   if (positions[1]) {
